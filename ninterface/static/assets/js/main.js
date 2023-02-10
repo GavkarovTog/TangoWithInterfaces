87057958265a5f2adb8d6e240863f9ab7ca33b0f
Denis <Shadow77x77@gmail.com>
2023-02-10 20:12:22 +0300

    Фитч с кнопкой на много раз

--- ninterface/static/assets/js/main.js
+++ ninterface/static/assets/js/main.js
@@ -1,20 +1,18 @@
 const btn = document.getElementById('getfile');
 
+btn.addEventListener('click', previewFile);
+
 function previewFile() {
     const content = document.querySelector('.text');
     const [file] = document.querySelector('input[type=file]').files;
     const reader = new FileReader();
-  
-    btn.addEventListener("click", () => {
-        console.log(typeof(reader.result));
-    }, false);
-  
+    console.log(typeof(reader.result));
     if (file) {
       reader.readAsText(file);
     }
     $.ajax({
         url: '',
-        type: 'POST',
+        type: 'post',
         data: {
             text: "Deniss"
         }
