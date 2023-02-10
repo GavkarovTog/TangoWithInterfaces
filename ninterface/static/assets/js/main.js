cbd4c16057cb491cee7f1b31762f23d6a056cc2f
Denis <Shadow77x77@gmail.com>
2023-02-10 18:11:04 +0300

    Сделал скрипт

--- ninterface/static/assets/js/main.js
+++ ninterface/static/assets/js/main.js
@@ -1,27 +1,15 @@
-const fileInput = document.getElementById("fileinput");
 const btn = document.getElementById('getfile');
 
-btn.addEventListener("click", () => {
-    const [file] = fileInput.files;
+function previewFile() {
+    const content = document.querySelector('.text');
+    const [file] = document.querySelector('input[type=file]').files;
+    const reader = new FileReader();
+  
+    btn.addEventListener("click", () => {
+        console.log(reader.result);
+    }, false);
+  
     if (file) {
-        const reader = new FileReader();
-        fileInput.addEventListener("load", () => {
-            console.log("test")
-            debugger
-            console.log(reader.result);
-        })
-        reader.readAsText(file);
+      reader.readAsText(file);
     }
 }
-)
-
-// fileInput.addEventListener("change", () => {
-//   const [file] = fileInput.files;
-//   if (file) {
-//     const reader = new FileReader();
-//     reader.addEventListener("load", () => {
-//       output.innerText = reader.result;
-//     });
-//     reader.readAsText(file);
-//   }
-// })
